95217e3fbf24cccaab5869c8745fcc3e82153585
Ahmad Bacchus <abacchus@outlook.com>
2018-01-25 10:13:00 -0500

    initial jenkins pipeline
    initial jenkins pipeline

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,206 @@
+pipeline {
+    agent none
+        environment {
+        DISABLE_AUTH = 'true'
+        DB_ENGINE    = 'sqlite'
+    }
+    stages {
+        /*Continuous Integration STARTED*/
+
+                stage('Cloning GIT Repo') {
+                    agent none
+                    steps {
+                        echo  ${env.BUILD_NUMBER}
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Code Scan') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Test Code Merge') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+
+                stage('Code Build') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Unit Test') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Publish Snapshot') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+
+            stage('DEV,QA and ET Deployments') {
+            parallel {
+                stage('Deploy snapshot to DEV') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+
+                stage('Deploy snapshot to QA') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Deploy snapshot to ET') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+            }
+    }
+        /*Continuous Delivery STARTED*/
+
+        stage('Continuous Delivery') {
+            parallel {
+                stage('Run ST Test') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Deploy to RT(UAT)') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Run ST RT(UAT)') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Deploy to Pre-Prod') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Run Performance Test') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+            }
+        }
+
+       /*DEPLOYMENTED STARTED*/ 
+        stage('Deployment') {
+            parallel {
+                stage('Deployment Approval') {
+                    agent none
+                    steps {
+                        input 'Deploy to prod?'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+                stage('Test On Linux') {
+                    agent none
+                    steps {
+                        echo 'post hello'
+                    }
+                    post {
+                        always {
+                            echo 'post hello'
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
