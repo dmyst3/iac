7e009b7a3c60019d6f4c6a1358873f10d75fd484
dmyst3 <dmyst3@yahoo.com>
2018-01-27 21:36:21 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,12 +5,22 @@ pipeline {
         string(defaultValue: 'Hello this is param 1', description: '', name: 'param1')
         string(defaultValue: 'Hello this is param 2', description: '', name: 'param2')
     }
+    environment {
+        MY_ENVAR1 = "My Env Var1"
+        MY_ENVAR2 = "My Env Var2"
+    }
 
     stages {
         stage("Print Params") {
             steps {
                 echo "flag: ${params.param1}"
                 echo "flag: ${params.param2}"
+            }
+        }
+        stage("Print ENV Vars") {
+            steps {
+                echo "MY_ENVAR1 is: $MY_ENVAR1"
+                echo "MY_ENVAR2 is: $MY_ENVAR2"
             }
         }
     }
