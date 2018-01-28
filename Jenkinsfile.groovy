a20978a54e3799a8eaede67bbf4aa13e9d9e2f33
dmyst3 <dmyst3@yahoo.com>
2018-01-27 21:26:51 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,27 +1,15 @@
 pipeline {
-  environment {
-    // environment variables and credential retrieval can be interspersed
-    SOME_VAR = "SOME VALUE"
-    // this assumes that "cred1" has been created on Jenkins Credentials
-    CRED1 = credentials("cred1")
-    INBETWEEN = "Something in between"
-    // this assumes that "cred2" has been created in Jenkins Credentials
-    CRED2 = credentials("cred2")
-    // Env variables can refer to other variables as well
-    OTHER_VAR = "${SOME_VAR}"
-  }
+    agent any
 
-  agent any
-
-  stages {
-    stage("print vars") {
-      steps {
-        // environment variables are not masked
-        echo "SOME_VAR is $SOME_VAR"
-        echo "INBETWEEN is $INBETWEEN"
-        echo "OTHER_VAR is $OTHER_VAR"
-
-      }
+    parameters {
+        booleanParam(defaultValue: true, description: '', name: 'userFlag')
     }
-  }
+
+    stages {
+        stage("foo") {
+            steps {
+                echo "flag: ${params.userFlag}"
+            }
+        }
+    }
 }
