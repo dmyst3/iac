bdaab3bc65472af8544f28ebdd44f13d85a08e76
dmyst3 <dmyst3@yahoo.com>
2018-01-28 11:51:23 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -34,5 +34,13 @@ pipeline {
                 echo "MY_Build_BUILD_URL is: ${env.BUILD_URL}"
             }
         }
+        stage('Using When') {
+            when {
+                $MY_ENVAR1 'My Env Var1'
+            }
+            steps {
+                echo "MY_ENVAR1 is set to : $MY_ENVAR1"
+            }
+        }
     }
 }
