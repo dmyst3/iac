7fe20e5baec170a77c470026f0401e47c917de16
dmyst3 <dmyst3@yahoo.com>
2018-01-27 21:29:25 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,14 +2,13 @@ pipeline {
     agent any
 
     parameters {
-        booleanParam(defaultValue: true, description: '', name: 'userFlag')
         string(defaultValue: true, description: '', name: 'test')
     }
 
     stages {
         stage("foo") {
             steps {
-                echo "flag: ${params.userFlag}"
+                echo "flag: ${params.test}"
             }
         }
     }
