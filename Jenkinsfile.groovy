8442862684a75b4672f742fe252fdcad35504e48
dmyst3 <dmyst3@yahoo.com>
2018-01-28 21:25:55 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,22 +22,22 @@ pipeline {
                     echo "param1: ${params.param1}"
                     echo "param2: ${params.param2}"
                     echo "Flag1: ${params.Flag1}"
-                }
             }
+        }
             stage("Print Custom Defined ENV Vars") {
                 steps {
                     echo "MY_ENVAR1 is: $MY_ENVAR1"
                     echo "MY_ENVAR2 is: $MY_ENVAR2"
                     echo "MY_Build_Number is: ${env.BUILD_NUMBER}"
-                }
             }
+        }
             stage("Print Jenkins ENV Vars") {
                 steps {
                     echo "MY_Build_Number is: ${env.BUILD_NUMBER}"
                     echo "MY_Build_JOB_NAME is: ${env.JOB_NAME}"
                     echo "MY_Build_BUILD_URL is: ${env.BUILD_URL}"
-                }
             }
+        }
             stage('Parallel Job') {
                 parallel  {
                     stage("Print Custom Defined ENV Vars1") {
@@ -45,31 +45,16 @@ pipeline {
                             echo "MY_ENVAR1 is: $MY_ENVAR1"
                             echo "MY_ENVAR2 is: $MY_ENVAR2"
                             echo "MY_Build_Number is: ${env.BUILD_NUMBER}"
-                        }
+                    }
                 }
                     stage("Print Jenkins ENV Vars1") {
                         steps {
                             echo "MY_Build_Number is: ${env.BUILD_NUMBER}"
                             echo "MY_Build_JOB_NAME is: ${env.JOB_NAME}"
                             echo "MY_Build_BUILD_URL is: ${env.BUILD_URL}"
-                        }
-                    }   
+                    }
                 }
             }
-               stage('Printing Job Result') {
-                    steps {
-                        echo "hello"
-                    }
-                    post {
-                        success {
-                            echo "Job Sucessfuly Passed"
-                        }
-                        failure {
-                            echo "Job Failed!"
-                        }
-                    }
-                   
-                }
-
+        }
     }
 }
