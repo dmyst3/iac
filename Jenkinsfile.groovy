dbca721a67cd260c88e464884cf17bf0ae0c1f62
dmyst3 <dmyst3@yahoo.com>
2018-01-28 21:27:22 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
     
     options{
-        buildDiscarder(logRotator(numToKeepStr:'4'))
+        buildDiscarder(logRotator(numToKeepStr:'5'))
         timeout(time: 30, unit: 'MINUTES')
     }
 
